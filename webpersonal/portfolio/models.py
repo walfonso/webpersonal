83ed8f45871632ffd549165aefaced9409e7a1f8
walfonso <walfonso@hotmail.com>
2019-01-04 00:46:43 -0300

    primer carga

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name= "Titulo")
+    decription = models.TextField(verbose_name= "Descripción")
+    image = models.ImageField(verbose_name= "Imagen", upload_to= "projects")
+    created = models.DateTimeField(auto_now_add=True, verbose_name= "Fecha de creación")
+    update = models.DateTimeField(auto_now=True, verbose_name= "Fecha de edición")
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
